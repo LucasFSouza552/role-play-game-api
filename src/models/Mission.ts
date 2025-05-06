91c0a1e856d8b4920c8eb68327a6aab0c49ea3a8
Thiago Gonçalves <thiago.goncalvesgames@gmail.com>
2025-05-06 16:26:03 -0300

    FEAT: Models das missões implementado.

--- src/models/Mission.ts
+++ src/models/Mission.ts
@@ -1,3 +1,11 @@
+import { MissionDifficult } from "./MissionDifficult";
+
 export interface Mission {
-    
+    id: number;
+    title: string;
+    description: string;
+    difficulty: MissionDifficult;
+    targetDate: Date;
+    XP: number;
+    money: number;
 }
