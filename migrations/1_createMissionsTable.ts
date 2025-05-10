74e2d9002f34b832ed5b205359347c8095047cc3
Thiago Gonçalves <thiago.goncalvesgames@gmail.com>
2025-05-10 13:04:43 -0300

    feat: Criada a tabela de Missions em Knex

---
+++ migrations/1_createMissionsTable.ts
@@ -0,0 +1,22 @@
+import { Knex } from "knex";
+import { MissionDifficult } from "../src/models/enums/MissionDifficult";
+
+const difficultyKeys = Object.values(MissionDifficult);
+
+const tablename = "missions";
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable(tablename, (table) => {
+        table.increments('id').primary();
+        table.string('title').notNullable();
+        table.text('description').notNullable();
+        table.enu('difficulty', difficultyKeys).notNullable();
+        table.date('targetDate').notNullable();
+        table.integer('xp');
+        table.decimal('money');
+    })
+}
+
+export async function down(knex: Knex) {
+    await knex.schema.dropTable(tablename);
+}
--- seeds/00_reset_all.ts
+++ seeds/00_reset_all.ts
@@ -4,6 +4,8 @@ export async function seed(knex: Knex): Promise<void> {
 
 await knex('champion_skills').del();
 
+await knex('missions').del();
+
 await knex('champions').del();
 
 await knex('skills').del();
@@ -13,4 +15,5 @@ await knex.raw('ALTER SEQUENCE champion_skills_id_seq RESTART WITH 1');
 await knex.raw('ALTER SEQUENCE champions_id_seq RESTART WITH 1');
 await knex.raw('ALTER SEQUENCE skills_id_seq RESTART WITH 1');
 await knex.raw('ALTER SEQUENCE champion_roles_id_seq RESTART WITH 1');
+await knex.raw('ALTER SEQUENCE missions_id_seq RESTART WITH 1');
 }
