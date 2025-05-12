3572e2fc053c7c4e64b74c007d708891cb717f34
Arthur Steiner <arthursteinerr@gmail.com>
2025-05-12 19:39:37 -0300

    feat: Adiciona migração e seed para a tabela de itens

---
+++ migrations/20250511150623_create_items_table.ts
@@ -0,0 +1,19 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("items", (table) => {
+        table.increments("id").primary(); // ID único
+        table.string("name").notNullable(); // Nome do item
+        table.text("description").notNullable(); // Descrição do item
+        table.enum("rarity", ["Common", "Uncommon", "Rare", "Epic", "Legendary"]).notNullable(); // Raridade
+        table.integer("level").notNullable(); // Nível necessário
+        table.decimal("priceMin", 10, 2).notNullable(); // Preço mínimo
+        table.decimal("priceMax", 10, 2).notNullable(); // Preço máximo
+        table.enum("type", ["Spells", "Armour", "Weapons", "Potions"]).notNullable(); // Tipo do item
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable("items");
+}
+
