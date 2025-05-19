b3bec48d786be50d76ede20ea104d69497a89570
PEDRO HENRIQUE CACADOR <1-23-14321@aluno.faminas.edu.br>
2025-05-19 20:18:45 -0300

    feat: GuildController getById

--- src/controllers/GuildController.ts
+++ src/controllers/GuildController.ts
@@ -1,17 +1,31 @@
 import { Request, Response } from 'express';
 import { GuildService } from '../services/GuildService';
-import { error } from 'console';
+import { guildRepo } from '../repositories/RepositoriosManager';
+
+const guildService = new GuildService();
 
 export class GuildController {
-    private guildService = new GuildService();
 
-    async findAll(req: Request, res: Response) {
+    async getAll(req: Request, res: Response) {
         try {
-            const guilds = await this.guildService.findAllGuild();
+            const guilds = await guildService.getAllGuild();
             res.json(guilds);
         } catch (error: any) {
             res.status(500).json({ error: error.message });
         }
     }
+
+    async getById(req: Request, res: Response) {
+        try {
+            const guildId = req.params.id;
+            res.json(guildId);
+        } catch (error: any) {
+            res.status(400).json({ error: error.message });
+        }
+
+    }
+
+
+
 
 }
