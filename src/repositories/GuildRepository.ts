aa32a69d37139ba2e8c6c4bcb0b1d8ff7cf9f9af
PedroCacador <gamermastermg26@gmail.com>
2025-05-19 16:16:08 -0300

    feat: add GuildRepository and GuildService

---
+++ src/repositories/GuildRepository.ts
@@ -0,0 +1,47 @@
+import db from "../database/db";
+import { Guild } from "../models/Guild";
+import { Filters } from "../models/Filters";
+
+export class GuildRepository {
+    private tablename = 'guild';
+
+    async findAll() {
+        return await db(this.tablename).select('*');
+    }
+
+    async findByGuildName(name: String) {
+        return await db(this.tablename).where({ name }).first();
+    }
+
+    async findById(id: number) {
+        return await db(this.tablename).where({ id }).first();
+    }
+
+    async create(guild: Guild) {
+        try {
+            const newGuild = await db(this.tablename).insert(guild).returning('*');
+            return newGuild;
+        } catch (error) {
+            throw new Error('Erro ao criar Guilda');
+        }
+    }
+
+    async update(id: number, guild: Guild) {
+        try {
+            const updatedGuild = await db(this.tablename).where({ id }).update(guild);
+            return updatedGuild;
+        } catch (error) {
+            throw new Error('Erro ao atualizar a Guilda');
+        }
+    }
+
+    async delete(id: number) {
+        try {
+            const deleteGuild = await db(this.tablename).where({ id }).del();
+            return deleteGuild;
+        } catch (error) {
+            throw new Error('Erro ao deletar a Guilda');
+        }
+    }
+
+}
