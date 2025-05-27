2055ffcd10b2b096270316ba75f6ecb555970fe8
LucasFSouza552 <lucasfelipe552@hotmail.com>
2025-05-27 00:26:59 -0300

    feat: adicionar seed para criação de inventários e implementar interface de inventário; criar serviço de inventário

--- src/models/Inventory.ts
+++ src/models/Inventory.ts
@@ -1,3 +1,11 @@
+import { Item } from "./Item";
+
+//Inventário padrão, pode ser usado por Champion, Guild ou Shop.
 export interface Inventory {
-    
+    id: number;                         // ID único do inventário
+    ownerId: number | string;           // ID do dono (Champion, Guild, Shop, etc)
+    ownerType: "champion" | "guild" | "shop"; // Tipo do dono (Champion, Guild, Shop)
+    capacity?: number;                  // Capacidade máxima de itens (opcional)
+    money?: number;                     // Moedas armazenadas no inventário (opcional)
+    lastUpdated?: Date;                 // Última atualização do inventário (opcional)
 }
